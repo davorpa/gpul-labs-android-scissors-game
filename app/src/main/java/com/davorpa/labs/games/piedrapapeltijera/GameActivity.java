75439371cbe97a368519b75fb90b60e0d4222587
davorpa <katelme@gmail.com>
2020-02-18 23:12:04 +0100

    Design basic skeleton UI for game activity

--- app/src/main/java/com/davorpa/labs/games/piedrapapeltijera/GameActivity.java
+++ app/src/main/java/com/davorpa/labs/games/piedrapapeltijera/GameActivity.java
@@ -3,6 +3,9 @@ package com.davorpa.labs.games.piedrapapeltijera;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -10,5 +13,40 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        final TextView txt_username = findViewById(R.id.txt_username);
+        txt_username.setText(getIntent().getStringExtra(MainActivity.EXTRA_USERNAME));
+    }
+
+    /**
+     * Método que se ejecuta al pulsar el botón Back
+     * @param view
+     */
+    public void onBackButton(final View view) {
+        onBackPressed();
+    }
+
+    /**
+     * Método que se ejecuta al pulsar el botón Stone
+     * @param view
+     */
+    public void onStoneButton(final View view) {
+        Toast.makeText(this, R.string.lbl_todo, Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * Método que se ejecuta al pulsar el botón Paper
+     * @param view
+     */
+    public void onPaperButton(final View view) {
+        Toast.makeText(this, R.string.lbl_todo, Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * Método que se ejecuta al pulsar el botón Scissors
+     * @param view
+     */
+    public void onScissorsButton(final View view) {
+        Toast.makeText(this, R.string.lbl_todo, Toast.LENGTH_LONG).show();
     }
 }
