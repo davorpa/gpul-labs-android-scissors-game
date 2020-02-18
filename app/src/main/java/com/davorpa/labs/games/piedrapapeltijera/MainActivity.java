40051fa85138bcc264d721cceb19cc02da18b2df
davorpa <katelme@gmail.com>
2020-02-18 17:52:56 +0100

    - Implements main_activity UI logic (link startActivity to same activity) - Refactor to less code (use activity.xml onClick instead of java listeners)

--- app/src/main/java/com/davorpa/labs/games/piedrapapeltijera/MainActivity.java
+++ app/src/main/java/com/davorpa/labs/games/piedrapapeltijera/MainActivity.java
@@ -12,12 +12,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String EXTRA_USERNAME =  BuildConfig.APPLICATION_ID + ".USERNAME";
-
-
-    private ImageButton btn_play;
-    private TextView lbl_play;
-    private EditText txt_username;
+    static final String EXTRA_USERNAME =  BuildConfig.APPLICATION_ID + ".USERNAME";
 
 
     @Override
@@ -26,34 +21,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Capture the layout's components
-        btn_play = findViewById(R.id.btn_play);
-        lbl_play = findViewById(R.id.lbl_play);
-        txt_username = findViewById(R.id.txt_username);
+        final EditText txt_username = findViewById(R.id.txt_username);
 
-        // Capture contexts
-        final Context thisContext = this;
+        //Init view with values provided by parent intent
+        final String username = getIntent().getStringExtra(MainActivity.EXTRA_USERNAME);
+        if (username != null) {
+            txt_username.setText(username);
+            txt_username.selectAll();
+            txt_username.requestFocus();
+        }
+    }
 
-        // Attach events
-        final View.OnClickListener playListener = new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-                // TODO: Find user in last players to set current user in game
-                // validate username
-                String username = txt_username.getText().toString();
-                if (username == null || username.trim().isEmpty()) {
-                    txt_username.setError(getString(R.string.err_required));
-                    txt_username.requestFocus();
-                    return;
-                }
 
-                // Navigate to other activity
-                // TODO: Navigate to GameActivity.class
-                //Intent intent = new Intent(thisContext, GameActivity.class);
-                //intent.putExtra(MainActivity.EXTRA_USERNAME, username);
-                //startActivity(intent);
-            }
-        };
-        btn_play.setOnClickListener(playListener);
-        lbl_play.setOnClickListener(playListener);
+    /**
+     * Método que se ejecuta al pulsar el botón Play
+     * @param view
+     */
+    public void onPlayClick(final View view) {
+        // Capture the layout's components
+        final EditText txt_username = findViewById(R.id.txt_username);
+
+        // TODO: Find user in last players to set current user in game
+        // validate username
+        final String username = txt_username.getText().toString();
+        if (username == null || username.trim().isEmpty()) {
+            txt_username.setError(getString(R.string.err_required));
+            txt_username.requestFocus();
+            return;
+        }
+
+        // Navigate to other activity
+        // TODO: Navigate to GameActivity.class
+        final Intent intent = new Intent(MainActivity.this, MainActivity.class);
+        intent.putExtra(MainActivity.EXTRA_USERNAME, username);
+        startActivity(intent);
     }
 }
