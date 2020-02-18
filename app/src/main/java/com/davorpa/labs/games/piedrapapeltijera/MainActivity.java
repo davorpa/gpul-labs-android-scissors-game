1980e70de129a2132d432581ba026d107cfafa86
davorpa <katelme@gmail.com>
2020-02-18 20:53:41 +0100

    Clean unused imports

--- app/src/main/java/com/davorpa/labs/games/piedrapapeltijera/MainActivity.java
+++ app/src/main/java/com/davorpa/labs/games/piedrapapeltijera/MainActivity.java
@@ -8,8 +8,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
